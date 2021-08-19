92cc3fe2b1be875b3c1bc92997babd3bf2456c57
tomoaki_mukaiyama <heymanyaman314@gmail.com>
2021-08-19 11:00:43 +0900

    edit layout

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,13 +1,14 @@
 import consumer from "./consumer"
 
 document.addEventListener('turbolinks:load', () => {
+  //部屋に入ると同時に入力フォームをフォーカス
   document.querySelector("#message_content").focus();
+  
   const room_element = document.getElementById("room-id");
   const room_id = room_element.getAttribute("data-room-id");
 
   //最新のメッセージへスクロール
   scrollToNewMessage();
-
   function scrollToNewMessage() {
     var nodes = document.querySelectorAll(".message.mb-2")
     nodes = Array.from(nodes)
@@ -17,11 +18,14 @@ document.addEventListener('turbolinks:load', () => {
     });
   }
 
+  //全部屋に同時に送信しないように部屋を移動するたびにサブスクを全削除
+  //応急処置
   consumer.subscriptions.subscriptions.forEach((sub) => {
     consumer.subscriptions.remove(sub)
   })
 
-  function countSpecifieChannel(channel, room) {
+  //チャンネルがサブスクされてる数を取得
+  function countSpecificChannel(channel, room) {
     var i = 0;
     var subscriptions = consumer.subscriptions.subscriptions;
     subscriptions.forEach(function (subscription) {
@@ -34,7 +38,9 @@ document.addEventListener('turbolinks:load', () => {
     return i;
   }
 
-  if (countSpecifieChannel("RoomChannel", room_id) === 1) {
+  //そのチャンネルがすでにサブスクされてたら新たに作成しない
+  //重複防止
+  if (countSpecificChannel("RoomChannel", room_id) === 1) {
     return;
   } else {
 
