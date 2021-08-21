524f59d69564aeb353fd21862e4974ba8d8ed7dc
tomoaki_mukaiyama <heymanyaman314@gmail.com>
2021-08-21 16:02:43 +0900

    broadcast namechange

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
   def update
+    preveous_username = set_user.username
     respond_to do |format|
       if set_user.update!(user_params)
         format.js
       end
     end
+    new_username = User.find_by(user_params).username
+    ActionCable.server.broadcast "room_channel_30", preveous_username: preveous_username, new_username: new_username, data_type: "username"
   end
 
 
